a2464e14b841bc05999f3a31c062321b6a06d569
taraspavliv <taras.pavliv@hotmail.com>
2021-12-02 23:30:57 +0100

    full code folder

---
+++ full code/main.py
@@ -0,0 +1,77 @@
+from camera import *
+from path_planning import *
+
+optimal_path = []
+
+
+capture = cv.VideoCapture('new.mp4')
+
+#for display
+frame_limits = []
+visibility_graph = []
+vertices = []
+
+def setup():
+    global capture, optimal_path, frame_limits, visibility_graph, vertices
+
+    valid_image = False
+    while not valid_image:
+        valid_image, first_image = capture.read()
+    
+    thymio_pos,targets_list,obstacles_list,dilated_obstacle_list,dilated_map, frame_limits = object_detection(first_image)
+
+    visibility_graph,start_idx,targets_idx_list,vertices = vis_graph(thymio_pos,targets_list,obstacles_list,dilated_obstacle_list,dilated_map)
+
+    distance_array, path_array = create_distance_path_matrix(visibility_graph,start_idx,[3,1])
+
+    total_distance, targets_idx_order = shortest_path(0, np.array([0]), distance_array)
+
+    print(total_distance)
+
+    for i in range(len(targets_idx_order)-1):
+        for j in range(len(path_array[i][i+1])):
+            if i != 0 and j == 0: 
+                continue
+            print(path_array[i][i+1][j])
+            optimal_path.append(vertices[path_array[i][i+1][j]])
+
+setup()
+
+print(optimal_path)
+
+thymio_cam_pos = []
+
+def cam_thread():
+    global capture, optimal_path, frame_limits, visibility_graph, vertices
+
+    show_contours = False
+    show_polygones = False
+    show_dilated_polygones = False
+    show_visibility_graph = False
+    show_option = [show_contours, show_polygones, show_dilated_polygones, show_visibility_graph]
+    while True:
+
+        valid_image, frame = capture.read()
+        if not valid_image:
+            break
+
+        bounded_frame = frame[frame_limits[0]: frame_limits[1], frame_limits[2]: frame_limits[3]]
+
+        modified_frame = draw_on_frame(bounded_frame, show_option)
+
+        cv.imshow('Video',modified_frame)
+        key_pressed = cv.waitKey(60)
+        if key_pressed == ord('q'):
+            show_contours = not show_contours
+        if key_pressed == ord('w'):
+            show_dilated_polygones = not show_dilated_polygones
+        if key_pressed == ord('e'):
+            show_visibility_graph = not show_visibility_graph
+        if key_pressed == ord('d'):
+            break
+
+        show_option = [show_contours, show_polygones, show_dilated_polygones, show_visibility_graph]
+
+cam_thread()
+
+
---
+++ full code/path_planning.py
@@ -0,0 +1,84 @@
+import numpy as np
+
+
+def dijkstra_aglorithm(idx_start, idx_end, visibility_graph):
+    path = []
+
+    if idx_start == idx_end:
+        return 0, np.array([])
+
+    nb_points = len(visibility_graph)
+    distance_array = np.full(nb_points, np.Inf, dtype=np.double) #creates an array to store distance to start
+    distance_array[idx_start] = 0
+    path_array = np.zeros(nb_points, dtype = int) #creates an array to store path from start
+    explored_array = np.full(nb_points, False, dtype=np.bool) #keeps track of explored points
+    explored_array[idx_start] = True
+    exploring_idx = idx_start #from where we are exploring
+
+    #find shortest path
+    while(explored_array[idx_end] == False):
+        for vertices in visibility_graph[exploring_idx]:
+            if distance_array[exploring_idx] + vertices[1] < distance_array[vertices[0]]:
+                distance_array[vertices[0]] = distance_array[exploring_idx] + vertices[1]
+                path_array[vertices[0]] = exploring_idx
+            
+        temp_idx = np.argmin(distance_array[np.logical_not(explored_array)])
+        exploring_idx = np.arange(nb_points)[np.logical_not(explored_array)][temp_idx]
+        explored_array[exploring_idx] = True
+
+    #store the optimal path
+    last_idx = idx_end
+    while last_idx != idx_start:
+        path.insert(0,last_idx)
+        last_idx = path_array[last_idx]
+    path.insert(0,idx_start)
+    
+    #path = np.array(path)
+    return distance_array[idx_end], path
+
+
+def create_distance_path_matrix(visibility_graph,start_idx,targets_idx_list):
+    #we treat start as an objective, from which we start
+    targets_idx_list.insert(0, start_idx)
+
+    nb_interest_point = len(targets_idx_list)
+    distance_matrix = np.zeros((nb_interest_point, nb_interest_point))
+    path_matrix = np.zeros((nb_interest_point, nb_interest_point), dtype=object)
+    for i in range(nb_interest_point):
+        for j in range(nb_interest_point):
+            if i==j:
+                distance_matrix[i][j] = 0
+                path_matrix[i][j] = [[targets_idx_list[i],targets_idx_list[i]]]
+            elif i > j:
+                distance_matrix[i][j], path_matrix[i][j] = dijkstra_aglorithm(targets_idx_list[i], targets_idx_list[j], visibility_graph)
+                distance_matrix[j][i] = distance_matrix[i][j]
+                path_matrix[j][i] = path_matrix[i][j][::-1]
+
+
+    return distance_matrix, path_matrix
+
+
+def shortest_path(start_local_idx, visited_idx_list, distance_array):
+    nb_points = np.size(distance_array,0) #number of points to go through + 1 for the starting point
+
+    distance = np.Inf #value to optimize
+    idx_list = np.zeros(np.size(distance_array,0)) #saves optimal path
+
+    if np.size(visited_idx_list) == nb_points : #end of recursion, all points have been passed through
+        return 0, np.zeros(np.size(distance_array,0))
+
+    temp_distance = 0
+    temp_idx = np.zeros(np.size(distance_array,0))
+
+    for i in range(nb_points):
+        if i in visited_idx_list : #tries all points that haven't been visited
+            continue
+        temp_distance, temp_idx = shortest_path(i, np.append(visited_idx_list, i), distance_array)
+        temp_distance += distance_array[start_local_idx][i]
+        
+        if temp_distance < distance : #if found a better way, updates its optimal path and value
+            distance = temp_distance
+            idx_list = temp_idx
+            idx_list[visited_idx_list.size] = i
+
+    return (distance, idx_list)
