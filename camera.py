ab2b2a27ebac1375b07b93825ce7fddf4d86a52e
tanguy7 <94747818+tanguy7@users.noreply.github.com>
2021-12-09 21:58:16 +0100

    Add files via upload
    With the rigth buffer number (bonne dilatation d'obstacle)

--- camera.py
+++ camera.py
@@ -76,9 +76,11 @@ def get_color_contour(frame, color):
     return contours
 
 
-def draw_analyze_frame(frame, show_options, dilated_obstacle_list, dilated_map, kalman_state, optimal_path_px):
+def draw_analyze_frame(frame, show_options, dilated_obstacle_list, dilated_map, visibility_graph, vertices, kalman_state, optimal_path_px, progress_idx):
     blurred_frame = cv.GaussianBlur(frame, (5,5), cv.BORDER_DEFAULT)
 
+    #show_option 0: show contours
+    #show_option 1: show estimated contours
     _, map_contour = cam_get_bounded_frame(blurred_frame, show_options[0], show_options[1])
     thymio_pos, thymio_angle, thymio_visible, thymio_radius, _ = cam_locate_thymio(blurred_frame, show_options[0], show_options[1])
     thymio_state = [thymio_pos[0], thymio_pos[1], thymio_angle]
@@ -86,6 +88,7 @@ def draw_analyze_frame(frame, show_options, dilated_obstacle_list, dilated_map, 
     cam_get_targets(blurred_frame, show_options[0], show_options[1])
     cam_get_obstacles(blurred_frame, thymio_radius, show_options[0], show_options[1])
 
+    #show_option 2: show dilated polygones
     if show_options[2]:
         for dilated_obstacle in dilated_obstacle_list:
             x_obst, y_obst = dilated_obstacle.exterior.xy
@@ -96,40 +99,57 @@ def draw_analyze_frame(frame, show_options, dilated_obstacle_list, dilated_map, 
         dilated_map_poly = list((int(point[0]),int(point[1])) for point in list(zip(x_map, y_map)))
         draw_polygone(blurred_frame, dilated_map_poly, "white")
 
+    #show_option 3: show visibility graph
+    if show_options[3]:
+        for i in visibility_graph:
+            for j in visibility_graph[i]:
+                cv.line(blurred_frame, [int(vertices[i][0]), int(vertices[i][1])] , [int(vertices[j[0]][0]), int(vertices[j[0]][1])], (100,100,100), 2)
+                #print(vertices[i], vertices[j[0]])
+
+    #show_option 5: show optimal path, and the progress on it
+    if show_options[5]:
+        draw_path(blurred_frame, optimal_path_px, progress_idx)
+
+    #show_option 4: show kalman estimated position (drawn last to be on top)
     if show_options[4]:
         kalman_pos = (kalman_state[0], kalman_state[1])
         cv.circle(blurred_frame, kalman_pos, 7, (255, 0, 255), -1)
-        print(kalman_state)
         cv.line(blurred_frame, kalman_pos, [int(kalman_pos[0] + 30*np.cos(kalman_state[2])), int(kalman_pos[1] - 30*np.sin(kalman_state[2]))], (255, 0, 255), 2)
-
-    if show_options[5]:
-        draw_polygone(blurred_frame, optimal_path_px, "yellow", False)
 
     return blurred_frame, thymio_state, thymio_visible
 
 
-def draw_polygone(frame, polygone_points, color, closed = True):
+def draw_polygone(frame, polygone_points, color):
     if color == "white":
         color_RGB = (255,255,255)
     elif color == "green":
         color_RGB = (0,255,0)
     elif color == "blue":
         color_RGB = (255,0,0)
-    elif color == "yellow":
-        color_RGB = (0,255,255)
-
 
     for i in range(len(polygone_points)-1):
         cv.line(frame, polygone_points[i], polygone_points[i+1], color_RGB, 3)
-    
-    if closed:
-        cv.line(frame, polygone_points[len(polygone_points)-1], polygone_points[0], color_RGB, 3)
+    cv.line(frame, polygone_points[i+1], polygone_points[0], color_RGB, 3)
 
+
+def draw_path(frame, path, progress_idx):
+    color_to_travel = (0,255,255) #yellow
+    color_traveled = (255,255,0) #cyan
+
+    for i in range(progress_idx):
+        cv.line(frame, path[i], path[i+1], color_traveled, 3)
+
+    for i in range(progress_idx,len(path)-1):
+        cv.line(frame, path[i], path[i+1], color_to_travel, 3)
 
 def cam_get_bounded_frame(frame, show_contour = False, show_polygone = False):
     contours_white = get_color_contour(frame, "white")
 
-    contour = max(contours_white, key = cv.contourArea)
+    if contours_white: #if list is not empty
+        contour = max(contours_white, key = cv.contourArea)
+        #print(contour)
+    else:
+        contour = [[0,0],[1,0]]
     x_rect,y_rect,w_rect,h_rect = cv.boundingRect(contour)
 
     frame_limits = [y_rect, y_rect+h_rect, x_rect, x_rect+w_rect] #min_y, max_y, min_x, max_x
@@ -140,7 +160,7 @@ def cam_get_bounded_frame(frame, show_contour = False, show_polygone = False):
         cv.drawContours(frame[frame_limits[0]: frame_limits[1], frame_limits[2]: frame_limits[3]], contour, -1, (255,255,255),3)
     
     if show_polygone:
-        white_pts = pts_white = np.squeeze(polygon(contour))
+        white_pts = np.squeeze(polygon(contour))
         draw_polygone(frame, white_pts, "white")
 
     return frame_limits, contour
@@ -174,7 +194,11 @@ def cam_locate_thymio(frame, show_contour = False, show_circle = False):
                 box = cv.boxPoints((rect_cent, (rect_width, rect_height), rect_angle))
                 box_points = list((int(point[0]),int(point[1])) for point in box)
                 draw_polygone(frame,box_points,"blue")
-            thymio_angle = np.arctan2(center[1] - rect_cent[1],rect_cent[0] - center[0]) #is in rad
+            #takes the angle from the rectangle framing the thymio that is the closest to the noisy angle
+            noisy_angle = np.arctan2(center[1] - rect_cent[1],rect_cent[0] - center[0]) #is in rad
+            possible_angles = np.array(list((-rect_angle + s*90.)*pi/180. for s in range(4)))
+            angles_errors = list(abs(np.arctan2(np.sin(noisy_angle - poss_angle), np.cos(noisy_angle - poss_angle))) for poss_angle in possible_angles)
+            thymio_angle = possible_angles[np.argmin(angles_errors)]
     thymio_visible = True
 
     if blue_zones_counter == 0:
@@ -185,7 +209,7 @@ def cam_locate_thymio(frame, show_contour = False, show_circle = False):
 
 def cam_get_contour(map_contour, radius):
     map_contour_poly = Polygon(np.squeeze(polygon(map_contour)))
-    dilated_map = map_contour_poly.buffer(-radius, join_style=3 ,single_sided=True)
+    dilated_map = map_contour_poly.buffer(-radius, join_style=2,single_sided=True)
     return map_contour_poly, dilated_map
 
 
@@ -220,7 +244,7 @@ def cam_get_obstacles(frame, radius, show_contour = False, show_polygone = False
         if area > 1000:
             pts = np.squeeze(polygon(contour_green))
             pol = Polygon(pts)
-            dilated_obstacle = pol.buffer(radius, join_style=3 ,single_sided=True)
+            dilated_obstacle = pol.buffer(radius, join_style=2 ,single_sided=True)
             dilated_obstacle_list.append(dilated_obstacle)
             area_obstacles = area_obstacles.union(dilated_obstacle)
             if show_contour:
