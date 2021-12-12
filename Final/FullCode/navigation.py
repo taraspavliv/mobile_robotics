f0b51ac6151c41727356efa429a57de67dc3e773
taraspavliv <taras.pavliv@hotmail.com>
2021-12-12 18:48:23 +0100

    navigation commented

--- Final/FullCode/navigation.py
+++ Final/FullCode/navigation.py
@@ -1,6 +1,11 @@
+"""
+navigation.py
+
+This file is used to ensure that the Thymio will go to a list of 2D coordinates in the right order
+"""
+
 import numpy as np
 from numpy.lib.function_base import angle
-
 
 THRESHOLD_POS=20    
 THRESHOLD_ANGLE=0.05
@@ -9,23 +14,48 @@ SPEED=100
 P=1
 D=0.5
 
-#Used to communicate with the Thymio in main
 def motors_command(motors):
+    """
+    motors_command creates the structure used to communicate with the Thymio
+
+    :motors:    The index 0 is the value calculated for the left motor 
+                and the index 1, for the right motor
+
+    :return:    The structure to communicate with the Thymio
+    """
+
     return {
         "motor.left.target": [motors[0]],
         "motor.right.target": [motors[1]],
     }
 
 
-#Returns the angle in 0-360 between two points
 def angle(P1,P2):
+    """
+    angle calculates the angle between the line given by two 2D points and the horizontal axis. 
+    The angles are given between 0 and 2pi, 0 being the horizontal axis
+
+    :P1:        The coordinates of the first point being the origin of the axis
+    :P2:        The second point to calculate the angle
+
+    :return:    The angle in [0,2pi]
+    """
     angle = np.arctan2(P2[1]-P1[1], P2[0]-P1[0])
     if angle < 0:
         angle = angle + 2*np.pi
     return angle
 
-#Returns True if the Thymio is on the left of its path
+
 def path_side(dep, obj, pos_r):
+    """
+    path_side returns True if the Thymio is on the left of its path and False otherwise
+
+    :dep:   The departure point of the Thymio
+    :obj:   The objective point of the Thymio
+    :pos_r: The position of the robot
+
+    :return: A boolean. False if the Thymio is on the right of its path, True otherwise
+    """
     v_path = np.array([obj[0]-dep[0], obj[1]-dep[1]])
     v_robot = np.array([pos_r[0]-dep[0], pos_r[1]-dep[1]])
     vect_prod = np.cross(v_path, v_robot)
@@ -36,26 +66,59 @@ def path_side(dep, obj, pos_r):
     else:
         return False
 
-#Returns the position error
+
 def error(pos_r, dep, obj):
+    """
+    error calculates the perpendicular distance between the line formed by the two 2D points dep and obj 
+    and the position of the robot
+
+    :pos_r:     The position of the robot
+    :dep:       The departure point of the Thymio
+    :obj:       The objective point of the Thymio
+
+    :return:    error, the distance from the position of the robot to the line
+    """
     error = np.linalg.norm(np.cross(obj-dep, dep-pos_r)) / np.linalg.norm(obj-dep)
     return error
 
-#When the objective is reached, one must receive a new objectif and the precedent objective becomes the departure position,
-#that's why the objectif number is incremented
-def reach_obj(pos_r, obj, objective_number, turn):    
-    if np.linalg.norm(pos_r-obj) <= THRESHOLD_POS:
-        turn=True
-        return True, objective_number+1, turn
-    else:
-        return False, objective_number, turn
 
-#When on an objectif, chooses the optimal side to turn
-#Returns True if the robot angle is bigger than the angle of the dep-obj on a trigonometric circle
+def reach_obj(pos_r, obj, objective_number, turn):
+    """
+    reach_obj detects when the Thymio is in the circle of center obj and radius THRESHOLD_POS. This is used to update
+    the number of objective reached and the Thymio state
+
+    :pos_r:             The position of the robot
+    :obj:               The objective point of the Thymio
+    :objective_number:  The number of objective the Thymio has already reached
+    :turn:              The state of the Thymio to know if the Thymio is moving forward (False) or turning (True)   
+
+    :return:            reached, boolean as True if the Thymio arrives on its objective, False otherwise
+                        objective_number, the number of objective already reached
+                        turn, the state of the Thymio to know if the Thymio is moving forward (False) or turning (True)
+    """
+    if np.linalg.norm(pos_r-obj) <= THRESHOLD_POS:
+        turn = True
+        reached = True
+        return reached, objective_number+1, turn
+    else:
+        reached = False
+        return reached, objective_number, turn
+
+
 def optimal_side(angle_r, dep, obj):
+    """
+    optimal_side calculates on which side it is better to turn when an objective is reached
+
+    :angle_r:   The angle of the robot in [0,2pi]
+    :dep:       The departure point of the Thymio
+    :obj:       The objective point of the Thymio
+
+    :return:    A boolean, True if the Thymio has to turn to the left
+    """
+
     rad=angle(dep, obj)
 
-    #if robot angle is on the left of the path's one
+    #if robot angle is bigger than the path's one
     if angle_r >= rad:
         if abs(angle_r - rad) <= abs(2*np.pi - angle_r + rad):
             return False
@@ -67,8 +130,20 @@ def optimal_side(angle_r, dep, obj):
         else:
             return False
 
-#When an objectif is reached, the robot turn on itself until it reaches the good angle.
+
 def start_angle(angle_r, dep, obj, turn):
+    """
+    start_angle ensures that the Thymio continues to turn until the angle is good enough when an objective is reached.
+    The rotation speed starts decreasing when the angle SLOWDOWN_ANGLE is reached.
+
+    :angle_r:   The angle of the robot in [0,2pi]
+    :dep:       The departure point of the Thymio
+    :obj:       The objective point of the Thymio
+    :turn:      The state of the Thymio to know if the Thymio is moving forward (False) or turning (True)
+
+    :return:    motors, the speed commands for the motors
+                turn, the state of the Thymio to know if the Thymio is moving forward (False) or turning (True)
+    """
     angle_dep_obj = angle(dep, obj)
     error_angle = angle_err(angle_r, angle_dep_obj)
     if abs(error_angle) <= THRESHOLD_ANGLE:
@@ -89,13 +164,30 @@ def start_angle(angle_r, dep, obj, turn):
         turn=True
     return motors, turn
 
-#Returns the angle error between two angles
+
 def angle_err(angle_1, angle_2):
+    """
+    angle_err calculates the error between two angles
+
+    :angle1:    The first angle
+    :angle2:    The second angle
+
+    :return:    err_angle, the error between two angles
+    """
     err_angle = np.arctan2(np.sin(angle_1 - angle_2), np.cos(angle_1 - angle_2))
     return err_angle
 
-#Returns the departure and objectif depending on the objective_number the Thymio has already reached
+
 def next_obj(obj_list, i):
+    """
+    next_obj returns the departure and objective depending on the objective_number the Thymio has already reached
+
+    :obj_list:  The list of the points to reach
+    :i:         The number of objective the Thymio has already reached
+
+    :return:    dep, the departure point
+                obj, the objective point
+    """
     dep=obj_list[i]
     if i+1 == len(obj_list):
         obj = obj_list[i]
@@ -103,8 +195,21 @@ def next_obj(obj_list, i):
         obj=obj_list[i+1]
     return dep, obj
 
-#PD regulator to correct the position when the Thymio is rolling between two objectives
+
 def motors_corr(prev_err_pos, err_pos, dep, obj, pos_r, T):
+    """
+    motors_corr calculates the speed correction using a PD regulator to correct the position when the
+    Thymio is rolling between two objectives
+
+    :prev_err_pos:  The previous position error
+    :err_pos:       The position error
+    :dep:           The departure point of the Thymio
+    :obj:           The objective point of the Thymio
+    :pos_r:         The position of the robot
+    :T:             The time between to calls of this function
+
+    :return:        motors, the speed commands for the motors
+    """
 
     speed_corr=err_pos*P + D*(err_pos-prev_err_pos)/T
     motors=np.array([0,0])
@@ -120,8 +225,26 @@ def motors_corr(prev_err_pos, err_pos, dep, obj, pos_r, T):
 
     return motors
 
-#Returns the motors commands one should send to the Thymio 
+
 def navigation(pos_r, angle_r, obj_list, prev_err_pos, T, objective_number, turn):
+    """
+    navigation returns the motors commands one should send to the Thymio to ensure the good displacement 
+    of the Thymio on the map
+
+    :pos_r:             The position of the robot
+    :angle_r:           The angle of the robot in [-Inf, Inf]
+    :obj_list:          The list of the points to reach   
+    :prev_err_pos:      The previous position error
+    :T:                 The time between to calls of this function
+    :objective_number:  The number of objective the Thymio has already reached
+    :turn:              The state of the Thymio to know if the Thymio is moving forward (False) or turning (True)
+
+    :return:            motors, the speed commands for the motors
+                        err_pos, the position error
+                        objective_number, the number of objective already reached
+                        turn, the state of the Thymio to know if the Thymio is moving forward (False) or turning (True)
+    """
+
     #Stops the Thymio at the end of its path
     if objective_number==len(obj_list)-1 :
         motors=np.array([0,0])
